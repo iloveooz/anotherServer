686409ae379d020d19ab7f203e37fa2d1efb44ed
iloveooz <iloveooz@yandex.ru>
2019-02-02 21:53:24 +0700

    start

---
+++ cmake-build-debug/CMakeFiles/anotherServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anotherServer.dir/main.cpp.o"
+  "anotherServer.pdb"
+  "anotherServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anotherServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
